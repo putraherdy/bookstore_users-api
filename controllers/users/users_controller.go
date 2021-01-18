29111fbdc0ad74170e0ac460f2b973de36bf0441
Herdy Dwi Putra <herdydwiputra@gmail.com>
2021-01-18 16:05:36 +0700

    dao structure

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,44 @@
+package users
+
+import (
+	"strconv"
+	"net/http"
+
+	"github.com/putraherdy/bookstore_users-api/services"
+	"github.com/putraherdy/bookstore_users-api/utils/errors"
+
+	"github.com/gin-gonic/gin"
+	"github.com/putraherdy/bookstore_users-api/domain/users"
+)
+
+func CreateUser(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
+}
+
+func GetUser(c *gin.Context) {
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
